b301888674219701bbf74a8ffb8e1d6eec05d8bf
unknown <frimpong.emman@northeastern.edu>
2024-09-27 13:38:46 -0400

    Firts Java Lab

---
+++ Hello World/src/hello/world/HelloWorld.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package hello.world;
+
+/**
+ *
+ * @author DELL
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int num1 = 20;
+        double num2 = 50;
+        for (int i = 0; i < 10; i++) {
+            num1++;
+            
+        }
+        System.out.println("Hello World");
+        System.out.println(num1);
+        // TODO code application logic here
+    }
+    
+}
